269135b7df7e1e6b3c5a46162c595226be9d4a53
LuanEcker <32625254+LuanEcker@users.noreply.github.com>
2018-07-13 23:20:18 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.0.111/lojavirtual/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.0.111/lojavirtual/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/WEBSERVER/Apache 2.2/htdocs/lojavirtual/catalog/');
+define('DIR_SYSTEM', 'C:/WEBSERVER/Apache 2.2/htdocs/lojavirtual/system/');
+define('DIR_LANGUAGE', 'C:/WEBSERVER/Apache 2.2/htdocs/lojavirtual/catalog/language/');
+define('DIR_TEMPLATE', 'C:/WEBSERVER/Apache 2.2/htdocs/lojavirtual/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/WEBSERVER/Apache 2.2/htdocs/lojavirtual/system/config/');
+define('DIR_IMAGE', 'C:/WEBSERVER/Apache 2.2/htdocs/lojavirtual/image/');
+define('DIR_CACHE', 'C:/WEBSERVER/Apache 2.2/htdocs/lojavirtual/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/WEBSERVER/Apache 2.2/htdocs/lojavirtual/system/storage/download/');
+define('DIR_LOGS', 'C:/WEBSERVER/Apache 2.2/htdocs/lojavirtual/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/WEBSERVER/Apache 2.2/htdocs/lojavirtual/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/WEBSERVER/Apache 2.2/htdocs/lojavirtual/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'alemao102030');
+define('DB_DATABASE', 'lojavirtual');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
